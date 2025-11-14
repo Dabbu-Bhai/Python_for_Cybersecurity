de34a590f7ba89910bec1ce974bd774cdf9c9028
Dabbbu-Bhai <divyanshupadhyay67000@gmail.com>
2025-11-14 22:31:32 +0530

    File handling in python

---
+++ file_handling.py
@@ -0,0 +1,23 @@
+# allows program to manage files - open(filename,mode)
+# with open("file.txt","r") as f:
+#     print("Read with r mode \n",f.read())
+
+# write mode removes all previous data 
+# with open("file.txt","w") as f:
+#     f.write("Written with write mode \n")
+
+# use append function to add to file 
+# with open("file.txt","a") as f:
+#     f.write("Written with append mode \n")
+
+
+# with open("file.txt","r+") as f: #if written with this then file data is erased
+#     # f.seek(6) #from where you fant to read the file
+#     pos = f.tell()
+#     content = f.read()
+#     print("Currently pointer is at :",pos,end="\n")
+#     print(content)
+
+with open("file.txt","r") as f:
+    content = f.read()
+    print(content)
